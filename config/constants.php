777092b055d69e6f56082c9f38fdecbdf79f5fcc
Wisal <wisal.sandeepa@gmail.com>
2021-12-29 11:12:17 +0530

    added db config file

---
+++ config/constants.php
@@ -0,0 +1,22 @@
+<?php
+	// Root url for the site
+	define('ROOT_URL', 'http://	localhost//BettingCenter-backend/');
+	
+	
+	// Database parameters
+	// Data source name
+	define('DSN', 'mysql:host=localhost;port=3306;dbname=bettingcenter');
+	
+	// Hostname
+	define('DB_HOST', 'localhost');
+	
+	// DB user
+	define('DB_USER', 'root');
+	
+	// DB password
+	// define('DB_PASSWORD', '');
+	define('DB_PASSWORD', '');
+	
+	// DB name
+	define('DB_NAME', 'bettingcenter');
+?>
