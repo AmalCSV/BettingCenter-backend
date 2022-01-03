ed48a73e08d7c1199d6b876380ad54633550cfb5
Wisal <wisal.sandeepa@gmail.com>
2022-01-03 08:34:31 +0530

    modified read file

--- users/read.php
+++ users/read.php
@@ -3,61 +3,35 @@
 header("Acess-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$id = '';
-$firstName = '';
-$lastName = '';
-$userName = '';
-$password = '';
-$isActive = '';
-
 //include database
 include_once "../config/constants.php";
 include_once "../config/database.php";
 
-//create query
-$query = "SELECT * FROM user ORDER BY id DESC";
 
-//prepare the query statement
-$stmt = $conn->prepare($query);
+if(isset($_GET['id'])){
 
-//execute the query
-$stmt->execute();
+    $id = $_GET['id'];
 
-$num = $stmt->rowCount();
+    //create query
+    $query = "SELECT firstName, lastName, userName FROM user WHERE id= :id AND isActive = 1 ";
 
-//check if more than zero users found
-if($num > 0){
-    //user array
-    $users_arr = array();
-    $users_arr["data"] = array(); 
-    
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-         extract($row);
-    
-         $user_record = array(
-             "id" => $id,
-             "firstName" =>$firstName,
-             "lastName" =>$lastName,
-             "userName" =>$userName,
-             "password" =>$password,
-             "isActive" =>$isActive
-         );
-    
-         //push to data
-         array_push($users_arr["data"], $user_record);
+    //prepare the query
+    $stmt = $conn->prepare($query);
+
+    //execute the query
+    $stmt->execute(['id'=>$id]);
+
+    if($stmt->rowCount() > 0) {
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        echo json_encode($row);
+
+    } else{
+
+        echo json_encode(array("message"=>"No Active User Found"));
     }
-    
-    //set response code - 200 OK
-    http_response_code(200);
-    //Turn to JSON and output (show users data in JSON format)
-    echo json_encode($users_arr);
-    
-    }else{
-    //set response code - 404 Not found
-    http_response_code(404);
-    //tell the user no Users found
-    echo json_encode(array("message"=>"No Users Found")
-    );
-    
-    }
-    ?>
+
+    $stmt->closeCursor();
+
+}
+
+
