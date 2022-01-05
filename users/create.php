c248ad2c37481280e60b440fc9a93ceee9c68004
Wisal <wisal.sandeepa@gmail.com>
2022-01-05 10:06:28 +0530

    modified files

--- users/create.php
+++ users/create.php
@@ -19,21 +19,41 @@ if(!property_exists($data, 'firstName') || $data->firstName =='null' || $data->f
             echo json_encode($data);
             exit();
 }
+
+if(!property_exists($data, 'lastName') || $data->lastName =='null' || $data->lastName == '') {
+    $data = ['message' => 'Invalid lastName', 'status' => "error"];
+            echo json_encode($data);
+            exit();
+}
+
+if(!property_exists($data, 'userName') || $data->userName =='null' || $data->userName == '') {
+    $data = ['message' => 'Invalid userName', 'status' => "error"];
+            echo json_encode($data);
+            exit();
+}
+
+if(!property_exists($data, 'password') || $data->firstName =='password' || $data->password == '') {
+    $data = ['message' => 'Invalid password', 'status' => "error"];
+            echo json_encode($data);
+            exit();
+}
   
     $firstName = $data->firstName;
     $lastName = $data->lastName;
     $userName = $data->userName;
     $password = $data->password;
+    $hashedPassword = md5($password);
 
     if(isset($firstName) && isset($lastName) && isset($userName) && isset($password)){
     $query = "INSERT INTO user (firstName, lastName, userName, password, isActive) VALUES (:firstName,:lastName,:userName,:password,1)";
     
     $stmt = $conn->prepare($query);
 
-    $stmt->execute(['firstName' => $firstName,'lastName' => $lastName,'userName' => $userName,'password' => $password]);
+    $stmt->execute(['firstName' => $firstName,'lastName' => $lastName,'userName' => $userName,'password' => $hashedPassword]);
 
-    $last_id = $stmt.lastrowid;
+    $last_id = $stmt.insert_id;
     
+    echo $last_id;
     //$last_id = 1;
     $querySelect = "SELECT id,firstName,lastName,userName FROM user WHERE  id = :id ";
 
--- users/login.php
+++ users/login.php
@@ -12,13 +12,13 @@ $password = '';
 
 $userName = isset($_GET["userName"]) ? $_GET["userName"] : die();
 
-$password = isset($_GET["password"]) ? $_GET["password"] : die();
+$password = md5(isset($_GET["password"]) ? $_GET["password"] : die());
 
-$loginQuery = "SELECT * FROM user WHERE userName = :userName AND password = :password";
+$loginQuery = "SELECT id, userName FROM user WHERE userName = :userName AND password = :password";
 
 $stmt = $conn->prepare($loginQuery);
 
-$stmt->execute(['userName' => $userName,'password' => $password,]);
+$stmt->execute(['userName' => $userName,'password' => $password]);
 
 if($stmt->rowCount() > 0){
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
