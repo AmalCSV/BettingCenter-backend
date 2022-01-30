98d63aac2d7e228d384c984a364b956fe0724ae1
Wisal <wisal.sandeepa@gmail.com>
2022-01-30 12:20:47 +0530

    added moidified settings create file

--- settings/create.php
+++ settings/create.php
@@ -12,53 +12,48 @@ $extendedJson = '';
 $data = json_decode(file_get_contents("php://input"));
 
 $id = $data->id;
+if ($data->id) {
+    $id = $data->id;
+ }
+
 $companyName = $data->companyName;
 $address = $data->address;
 $tax = $data->tax;
-$extendedJson = $data->extendedJson;
 
-if(isset($id) && isset($companyName) && isset($address) && isset($tax) && isset($extendedJson)){
+$extendedJson = null;
+ if ($data->extendedJson) {
+    $extendedJson = $data->extendedJson;
+ }
 
-$queryExist = " SELECT * FROM settings WHERE id = :id ";
-    $stmtExist = $conn->prepare($queryExist);
-    $stmtExist->execute(['id' => $id]);
+if(isset($companyName) && isset($address) && isset($tax)){
+    $companyName = htmlspecialchars(strip_tags($companyName));
+    $address = htmlspecialchars(strip_tags($address));
+    $tax = htmlspecialchars(strip_tags($tax));
+    $extendedJson = htmlspecialchars(strip_tags($extendedJson));
 
-        if( $stmtExist->rowCount() >0 ){
+    if($id == null) {
+        $queryInsert = "INSERT INTO settings (companyName,address,tax,extendedJson) VALUES (:companyName,:address,:tax,:extendedJson)";
+        $stmtInsert = $conn->prepare($queryInsert);
+     
+        $stmtInsert->execute(['companyName' =>$companyName, 'address' =>$address, 'tax' =>$tax, 'extendedJson' =>$extendedJson]);
+        echo json_encode(array("success" => true, "message" => "Settings was created"));
+    } else {
+        $queryExist = " SELECT * FROM settings WHERE id = :id ";
+        $stmtExist = $conn->prepare($queryExist);
+        $stmtExist->execute(['id' => $id]);
+        
+        if( $stmtExist->rowCount() > 0 ){
             $queryUpdate = "UPDATE settings SET companyName = :companyName, address = :address, tax = :tax, extendedJson = :extendedJson WHERE id = :id";
             $stmtUpdate = $conn->prepare($queryUpdate);
-
-            //sanitize data(clean up for security)
             $id = htmlspecialchars(strip_tags($id));
-            $companyName = htmlspecialchars(strip_tags($companyName));
-            $address = htmlspecialchars(strip_tags($address));
-            $tax = htmlspecialchars(strip_tags($tax));
-            $extendedJson = htmlspecialchars(strip_tags($extendedJson));
-      
-            
             $stmtUpdate->execute(['id' =>$id, 'companyName' =>$companyName, 'address' =>$address, 'tax' =>$tax, 'extendedJson' =>$extendedJson ]);
-
-            echo json_encode(array("message" => "Settings was updated"));
-
-        }else{
-            $queryInsert = "INSERT INTO settings (id,companyName,address,tax,extendedJson) VALUES (:id,:companyName,:address,:tax,:extendedJson)";
-            $stmtInsert = $conn->prepare($queryInsert);
-
-            //sanitize data(clean up for security)
-            $id = htmlspecialchars(strip_tags($id));
-            $companyName = htmlspecialchars(strip_tags($companyName));
-            $address = htmlspecialchars(strip_tags($address));
-            $tax = htmlspecialchars(strip_tags($tax));
-            $extendedJson = htmlspecialchars(strip_tags($extendedJson));
-         
-            $stmtInsert->execute(['id' =>$id, 'companyName' =>$companyName, 'address' =>$address, 'tax' =>$tax, 'extendedJson' =>$extendedJson]);
-
-            echo json_encode(array("message" => "Settings was created"));
-
+            echo json_encode(array("success" => true, "message" => "Settings was updated"));
+        }else {
+            echo json_encode(array( "success" => false, "message" => "No record for Id"));
         }
+
     }
-
-    
-
-        
-    
-    
+} else {
+    echo json_encode(array( "success" => false, "message" => "No record for requried fields"));
+}
+?>
