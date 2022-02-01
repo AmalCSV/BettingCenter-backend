5e40bc7b27a8cd6a1791b77fcd4f5c3becfc70a2
Wisal97 <41443588+Wisal97@users.noreply.github.com>
2022-02-01 07:29:35 +0530

    Update 1.sql

--- db/1.sql
+++ db/1.sql
@@ -1,3 +1,7 @@
+--2022-02-01
+ALTER TABLE `racewinninghorse` CHANGE `amount` `amountFront` INT(11) NOT NULL;
+ALTER TABLE `racewinninghorse` ADD `amountBack` INT NOT NULL AFTER `amountFront`;
+
 -- 2022-01-30
 ALTER TABLE `raceWinning` CHANGE `raceIdentifier` `raceCode` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
 
