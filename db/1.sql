f869b37568de70e2601062e9e84b3e09b6e8960c
AmalCSV <88196854+AmalCSV@users.noreply.github.com>
2022-03-12 21:35:18 +1100

    fixed sql order

--- db/1.sql
+++ db/1.sql
@@ -1,9 +1,14 @@
---2022-02-01
-ALTER TABLE `racewinninghorse` CHANGE `amount` `amountFront` INT(11) NOT NULL;
-ALTER TABLE `racewinninghorse` ADD `amountBack` INT NOT NULL AFTER `amountFront`;
 
 -- 2022-01-30
 ALTER TABLE `raceWinning` CHANGE `raceIdentifier` `raceCode` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
 
 CREATE TABLE `raceWinningHorseAmount` ( `id` INT NOT NULL AUTO_INCREMENT ,
  `bettingHorseId` INT NOT NULL , `amountTypeId` INT NOT NULL , `amount` DOUBLE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+
+--2022-02-01
+ALTER TABLE `racewinninghorse` CHANGE `amount` `amountFront` INT(11) NOT NULL;
+ALTER TABLE `racewinninghorse` ADD `amountBack` INT NOT NULL AFTER `amountFront`;
+
+
+ALTER TABLE `raceWinningHorse` CHANGE `winingPlace` `winningPlace` INT(11) NOT NULL;
