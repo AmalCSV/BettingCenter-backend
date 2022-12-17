32af96f7ad7d89605d6a4a9bd67c833478d8b740
harith2001 <danula243@gmail.com>
2022-12-17 06:36:51 +0530

    calculation

--- betting/bettingcalculation.php
+++ betting/bettingcalculation.php
@@ -1,61 +1,88 @@
+
 <?php
 
-//deaclare varibles
+function bettingCalculation($bets){
 
-$front_money = 20;
-$back_money = 10;
-$no_of_horses = 4;
-$betting_charge = 5;
-$total_betting_charge;
+$bettintgAmount = prepareBettingAmount($bets);
+
+$finalAmount = getBettingAmount($bettingAmountArray => frontMoney,backMoney);
+
+return $finalAmount;
+
+}
+
+
+
+function prepareBettingAmount($bets) {
+
+    $noOfHorses = 0;
+  foreach ($bets as $bet) {
+  
+    $noOfHorses = $bet->bettingHorse -> count;
+    
+  }
+  
+  $bettingcen_record = array(
+             "front_money" => frontMoney;
+             "backAmount" =>$backMoney;
+         );
+         
+  return $bettingcen_record;
+
+$frontMoney;
+backMoney,
+$noOfHorses,
+$bettingCharge
 
 //double
-$double_win;
+$doubleWin;
 $double_1 = 1;
 $double_2 = 1;
-$double_fact = ($no_of_horses) - 2 ;
+$doubleFact = ($noOfHorses) - 2 ;
 //tribble
-$tribble_win;
+$tribbleWin;
 $tribble_1 = 1;
 $tribble_2 = 1;
-$tribble_fact = ($no_of_horses) - 3 ;
+$tribbleFact = ($noOfHorses) - 3 ;
 
 
 //double probability
-for ($i=$no_of_horses; $i >= 1 ; $i--) {
+for ($i=$noOfHorses; $i >= 1 ; $i--) {
 
      $double_1 =  $double_1 * $i;
 }
 
 
-for ($i=$double_fact; $i >= 1 ; $i--) {
+for ($i=$doubleFact; $i >= 1 ; $i--) {
 
     $double_2 =  $double_2 * $i;
 }
 
-$double_win = $double_1 / (2 * $double_2);
+$doubleWin = $double_1 / (2 * $double_2);
 
 //tribble probability
-for ($i=$no_of_horses; $i >= 1 ; $i--) {
+for ($i=$noOfHorses; $i >= 1 ; $i--) {
 
     $tribble_1 =  $tribble_1 * $i;
 }
 
 
-for ($i=$tribble_fact; $i >= 1 ; $i--) {
+for ($i=$tribbleFact; $i >= 1 ; $i--) {
 
    $tribble_2 =  $tribble_2 * $i;
 }
 
 
-$tribble_win = $tribble_1 / (6 * $tribble_2);
+$tribbleWin = $tribble_1 / (6 * $tribble_2);
 
 
 //calculations 
 
-$total_betting_charge = $front_money + $back_money + ($betting_charge * $double_win) + ($betting_charge * $tribble_win);
+return $totalBettingCharge = $frontMoney + $backMoney + ($bettingCharge * $doubleWin) + ($bettingCharge * $tribbleWin);
 
-//output
+}
 
-echo $total_betting_charge;
-
+echo bettingCalculation(20,10,4,5);
 ?>
+
+
