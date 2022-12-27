1d329d6001f4d380934976e8243479166d949859
harith2001 <danula243@gmail.com>
2022-12-27 17:12:47 +0530

    bettingCalculation

--- betting/bettingcalculation.php
+++ betting/bettingcalculation.php
@@ -3,9 +3,8 @@
 
 function bettingCalculation($bets){
 
-$bettintgAmount = prepareBettingAmount($bets);
-
-$finalAmount = getBettingAmount($bettingAmountArray => frontMoney,backMoney);
+$bettintgAmount = prepareBettingAmount($frontMoney, $backMoney, $bettingCharge,$doubleWin,$tribbleWin);
+$finalAmount = getBettingAmount();
 
 return $finalAmount;
 
@@ -13,76 +12,110 @@ return $finalAmount;
 
 
 
-function prepareBettingAmount($bets) {
+function prepareBettingAmount($frontMoney, $backMoney, $bettingCharge,$doubleWin,$tribbleWin){
 
-    $noOfHorses = 0;
+  $noOfHorses = 0;
+
   foreach ($bets as $bet) {
   
     $noOfHorses = $bet->bettingHorse -> count;
     
   }
-  
-  $bettingcen_record = array(
-             "front_money" => frontMoney;
-             "backAmount" =>$backMoney;
+ 
+  return  array (
+
+             "frontMoney"-> $frontMoney, 
+             "backMoney" -> $backMoney,
+             "bettingCharge" -> $bettingCharge,
+             "doubleWin" -> $doubleWin,
+             "tribbleWin"-> $tribbleWin,
          );
          
-  return $bettingcen_record;
-
-$frontMoney;
-backMoney,
-$noOfHorses,
-$bettingCharge
-
-//double
-$doubleWin;
-$double_1 = 1;
-$double_2 = 1;
-$doubleFact = ($noOfHorses) - 2 ;
-//tribble
-$tribbleWin;
-$tribble_1 = 1;
-$tribble_2 = 1;
-$tribbleFact = ($noOfHorses) - 3 ;
-
-
-//double probability
-for ($i=$noOfHorses; $i >= 1 ; $i--) {
-
-     $double_1 =  $double_1 * $i;
 }
 
 
-for ($i=$doubleFact; $i >= 1 ; $i--) {
-
-    $double_2 =  $double_2 * $i;
-}
-
-$doubleWin = $double_1 / (2 * $double_2);
-
-//tribble probability
-for ($i=$noOfHorses; $i >= 1 ; $i--) {
-
-    $tribble_1 =  $tribble_1 * $i;
-}
-
-
-for ($i=$tribbleFact; $i >= 1 ; $i--) {
-
-   $tribble_2 =  $tribble_2 * $i;
-}
+//
+function getBettingAmount($frontMoney, $backMoney, $bettingCharge, $noOfHorses,$doubleWin,$tribbleWin) {
+  
+  /*
+  $frontMoney = 20;
+  $backMoney = 10;
+  $bettingCharge = 5;
+  $noOfHorses = 4;
+  $doubleWin = 1;
+  $tribbleWin = 2;
+*/
 
 
-$tribbleWin = $tribble_1 / (6 * $tribble_2);
+  try {
+    //double calculation 
+    if ($doubleWin == 1) {
+
+      $double_1 = 1;
+      $double_2 = 1;
+      $doubleFact = ($noOfHorses) - 2;
+
+
+      //double probability
+      for ($i = $noOfHorses; $i >= 1; $i--) {
+
+        $double_1 = $double_1 * $i;
+      }
+
+
+      for ($i = $doubleFact; $i >= 1; $i--) {
+
+        $double_2 = $double_2 * $i;
+      }
+
+      $double = $double_1 / (2 * $double_2);
+
+    }
+  }catch (Exception $e){
+     echo "double error". $e -> getMessage();
+  }
+
+
+  try {
+    if ($tribbleWin == 2) {
+
+      $tribble_1 = 1;
+      $tribble_2 = 1;
+      $tribbleFact = ($noOfHorses) - 3;
+
+
+
+
+      //tribble probability
+      for ($i = $noOfHorses; $i >= 1; $i--) {
+
+        $tribble_1 = $tribble_1 * $i;
+      }
+
+
+      for ($i = $tribbleFact; $i >= 1; $i--) {
+
+        $tribble_2 = $tribble_2 * $i;
+      }
+
+
+      $tribble = $tribble_1 / (6 * $tribble_2);
+    }
+  }catch(Exception $e){
+    echo "Tribble  Error" . $e->getMessage();
+  }
 
 
 //calculations 
 
-return $totalBettingCharge = $frontMoney + $backMoney + ($bettingCharge * $doubleWin) + ($bettingCharge * $tribbleWin);
+ $totalBettingCharge = $frontMoney + $backMoney + ($bettingCharge * $double) + ($bettingCharge * $tribble);
+
+return $totalBettingCharge;
 
 }
 
-echo bettingCalculation(20,10,4,5);
+echo getBettingAmount();
+
 ?>
 
 
