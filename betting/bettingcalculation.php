5481cf8fd705d2e07f6425edac48132d2c3a9e38
harith2001 <danula243@gmail.com>
2023-01-06 18:02:10 +0530

    Updated Betting Calculation

--- betting/bettingcalculation.php
+++ betting/bettingcalculation.php
@@ -1,9 +1,35 @@
 
 <?php
+/*
+{
+    "customer": "1234",
+    "bettingDate": "2022-01-20",
+    "bettingCenterId": "02",
+    "bettingAmount": "3500",
+    "createdBy": "31",
+    "bets": [
+        {"bettingHorse": [{
+            "raceCode":"w1", "horseCode": "Fine"
+            }, 
+            {
+            "raceCode":"w3", "horseCode": "Fine 32"
+            },
+            {
+            "raceCode":"w2", "horseCode": "DEC"
+            }],
+        "amounts": [{"amountTypeId":2, 
+        "amount": 100},
+        {"amountTypeId":1, 
+        "amount": 20}]
+        }
+    ],
+    "amountTypeId": 1, 
+    "amount": 20
+}
+*/
+function bettingCalculation($bet){
 
-function bettingCalculation($bets){
-
-$bettintgAmount = prepareBettingAmount($frontMoney, $backMoney, $bettingCharge,$doubleWin,$tribbleWin);
+$bettintgAmount = prepareBettingAmount($bet);
 $finalAmount = getBettingAmount();
 
 return $finalAmount;
@@ -11,15 +37,37 @@ return $finalAmount;
 }
 
 
-
-function prepareBettingAmount($frontMoney, $backMoney, $bettingCharge,$doubleWin,$tribbleWin){
+function prepareBettingAmount($bet){
 
   $noOfHorses = 0;
+  $frontMoney = 0;
+  $backMoney = 0;
+  $bettingCharge = 0;
 
-  foreach ($bets as $bet) {
-  
-    $noOfHorses = $bet->bettingHorse -> count;
-    
+
+  foreach ($bet as $bet) {
+
+    $bettingAmount = $bet->amount;
+     foreach($bettingAmount as $bettingAmount){
+      if ($bettingAmount->amountTypeId == 1){
+        $frontMoney = $frontMoney + $bettingAmount->amount;
+      }
+      else if ($bettingAmount -> amountTypeId == 2){
+        $backMoney = $backMoney + $bettingAmount->amount;
+      }
+      else if ($bettingAmount -> amountTypeId == 3){
+        $doubleWin = $doubleWin + $bettingAmount->amount;
+      }
+      else if ($bettingAmount -> amountTypeId == 4){
+        $tribbleWin = $tribbleWin + $bettingAmount->amount;
+      }
+
+      $noOfHorses = $bet->bettingHorse -> count;
+
+      $bettingCharge = $bet->bettingAmount;
+
+     }
+
   }
  
   return  array (
@@ -42,26 +90,24 @@ function getBettingAmount($frontMoney, $backMoney, $bettingCharge, $noOfHorses,$
   $backMoney = 10;
   $bettingCharge = 5;
   $noOfHorses = 4;
-  $doubleWin = 1;
-  $tribbleWin = 2;
+  $doubleWin = 3;
+  $tribbleWin = 4;
 */
 
 
   try {
     //double calculation 
-    if ($doubleWin == 1) {
+    if ($doubleWin == 3) {
 
       $double_1 = 1;
       $double_2 = 1;
       $doubleFact = ($noOfHorses) - 2;
-
 
       //double probability
       for ($i = $noOfHorses; $i >= 1; $i--) {
 
         $double_1 = $double_1 * $i;
       }
-
 
       for ($i = $doubleFact; $i >= 1; $i--) {
 
@@ -75,16 +121,12 @@ function getBettingAmount($frontMoney, $backMoney, $bettingCharge, $noOfHorses,$
      echo "double error". $e -> getMessage();
   }
 
-
   try {
-    if ($tribbleWin == 2) {
+    if ($tribbleWin == 4) {
 
       $tribble_1 = 1;
       $tribble_2 = 1;
       $tribbleFact = ($noOfHorses) - 3;
-
-
-
 
       //tribble probability
       for ($i = $noOfHorses; $i >= 1; $i--) {
@@ -92,19 +134,16 @@ function getBettingAmount($frontMoney, $backMoney, $bettingCharge, $noOfHorses,$
         $tribble_1 = $tribble_1 * $i;
       }
 
-
       for ($i = $tribbleFact; $i >= 1; $i--) {
 
         $tribble_2 = $tribble_2 * $i;
       }
-
 
       $tribble = $tribble_1 / (6 * $tribble_2);
     }
   }catch(Exception $e){
     echo "Tribble  Error" . $e->getMessage();
   }
-
 
 //calculations 
 
@@ -113,8 +152,6 @@ function getBettingAmount($frontMoney, $backMoney, $bettingCharge, $noOfHorses,$
 return $totalBettingCharge;
 
 }
-
-echo getBettingAmount();
 
 ?>
 
